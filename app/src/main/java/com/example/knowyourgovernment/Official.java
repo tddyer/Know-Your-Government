f0ad167ec42401e1c38ec353518b83e64c60b0f7
Tanner <tdyer@hawk.iit.edu>
2020-11-03 16:57:02 -0600

    created official class

---
+++ app/src/main/java/com/example/knowyourgovernment/Official.java
@@ -0,0 +1,33 @@
+package com.example.knowyourgovernment;
+
+public class Official {
+
+    String title;
+    String name;
+    String party;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+}
