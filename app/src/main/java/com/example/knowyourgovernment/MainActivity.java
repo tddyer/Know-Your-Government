14d321652297d23e310e739bade8cc54c61434ea
Tanner <tdyer@hawk.iit.edu>
2020-11-04 17:38:48 -0600

    added about activity

--- app/src/main/java/com/example/knowyourgovernment/MainActivity.java
+++ app/src/main/java/com/example/knowyourgovernment/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.knowyourgovernment;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
     implements View.OnClickListener, View.OnLongClickListener {
@@ -73,14 +80,37 @@ public class MainActivity extends AppCompatActivity
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menuInfoItem:
-                // launch about activity here
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
                 return true;
             case R.id.menuLocationItem:
                 // open alert dialog that allows user to enter city/state or zip to update location
+                updateLocation();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    // alert dialogs
+
+    public void updateLocation() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_NUMBER);
+        input.setGravity(Gravity.CENTER);
+        builder.setView(input);
+        builder.setPositiveButton("OK", (dialog, id) -> {
+            // TODO: Update location here
+        });
+        builder.setNegativeButton("CANCEL", (dialog, id) -> {
+            // do nothing
+        });
+//        builder.setTitle("Stock Selection");
+        builder.setMessage("Enter a City, State or a Zip Code: ");
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 
